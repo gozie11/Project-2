fe703b9ba6ec5b4f34aea61a3a7bdd5e86513c8a
benhprentice <jabberthok@yahoo.com>
2022-04-02 17:22:06 -0500

    Submit Resume

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from urllib import request
-
-from flask import Flask, render_template
-from flask_bootstrap5 import Bootstrap
-import PyPDF2
-
-from werkzeug.utils import secure_filename
 import os
 from os import abort
+from urllib import request
+
+import PyPDF2
+from flask import Flask, render_template, request
+from flask_bootstrap5 import Bootstrap
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['UPLOAD_EXTENSIONS']=['.pdf']
+app.config['UPLOAD_PATH'] = 'data'
 bootstrap = Bootstrap(app)
 
 user = {}
@@ -16,42 +17,10 @@ user = {}
 
 @app.route('/')
 def index():  # put application's code here
-    pdfFileObj = open('static/sample_resume.pdf', 'rb')
-    # Creating a pdf reader object
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    # Getting number of pages in pdf file
-    pages = pdfReader.numPages
-    # Loop for reading all the Pages
-    for i in range(pages):
-        # Creating a page object
-        pageObj = pdfReader.getPage(i)
-        # Printing Page Number
-        print("Page No: ", i)
-        # Extracting text from page
-        # And splitting it into chunks of lines
-        text = pageObj.extractText().split('\n')
-        # Finally the lines are stored into list
-        # For iterating over list a loop is used
-        user['name'] = text[5]
-        user['email'] = text[18]
-
-        '''for i in range(len(text)):
-            # Printing the line
-            # Lines are seprated using "\n"
-            print(text[i], end="\n")
-            # For Seprating the Pages
-        '''
-    # closing the pdf file object
-    pdfFileObj.close()
     return render_template("index.html", user=user)
 
 
-@app.route('/upload/')
-def actors():
-    return render_template('upload.html')
-
-
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload/', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
         uploaded_file = request.files['file']
@@ -61,6 +30,7 @@ def upload():
         if file_ext not in app.config['UPLOAD_EXTENSIONS']:
             abort(400)
         uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+        return render_template('display.html')
     return render_template('upload.html')
 
 
