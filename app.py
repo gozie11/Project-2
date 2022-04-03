49458fa697b389b035276a6a80bb91a3293bdec9
benhprentice <jabberthok@yahoo.com>
2022-04-03 09:37:23 -0500

    Cleanup redirect

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from os import abort
 from urllib import request
 
 import PyPDF2
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from flask_bootstrap5 import Bootstrap
-from werkzeug.utils import secure_filename
+from werkzeug.utils import secure_filename, redirect
 
 app = Flask(__name__)
 app.config['UPLOAD_EXTENSIONS']=['.pdf']
@@ -30,12 +30,12 @@ def upload():
         if file_ext not in app.config['UPLOAD_EXTENSIONS']:
             abort(400)
         uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-        return display()
+        return redirect(url_for("display", usr = user ))
     return render_template('upload.html')
 
 
-@app.route('/display/')
-def display():
+@app.route('/<usr>')
+def display(usr):
     pdfFileObj = open('data/sample_resume.pdf', 'rb')
     # Creating a pdf reader object
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
