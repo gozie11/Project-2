498ac1606aecf0140b4e4bcb7bfade6bcded3533
Chigozie Onyia <cpo21@txstate.edu>
2022-04-10 21:05:27 -0500

    Added a text file to get the output of the application

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_bootstrap5 import Bootstrap
 from werkzeug.utils import secure_filename, redirect
 
 app = Flask(__name__)
-app.config['UPLOAD_EXTENSIONS']=['.pdf']
+app.config['UPLOAD_EXTENSIONS'] = ['.pdf']
 app.config['UPLOAD_PATH'] = 'data'
 bootstrap = Bootstrap(app)
 
@@ -30,7 +30,7 @@ def upload():
         if file_ext not in app.config['UPLOAD_EXTENSIONS']:
             abort(400)
         uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-        return redirect(url_for("display", usr = user ))
+        return redirect(url_for("display", usr=user))
     return render_template('upload.html')
 
 
@@ -52,23 +52,22 @@ def display(usr):
         text = pageObj.extractText().split('\n')
         # Finally the lines are stored into list
         # For iterating over list a loop is used
+
         user['name'] = text[5:9]
         user['address'] = text[10:13]
         user['phonenum'] = text[14:17]
         user['email'] = text[18]
         user['info'] = text[19:]
 
-        #print(user['name'])
+        # print(user['name'])
 
         phonenum = ''.join(user['phonenum'])
         address = ''.join(user['address'])
         name = ''.join(user['name'])
         info = ''.join(user['info'])
-
+        email = ''.join(user['email'])
 
         # make a list using all the elements in user['name]
-
-
 
         for i in range(len(text)):
             # Printing the line
@@ -76,9 +75,22 @@ def display(usr):
             print(text[i], end="\n")
             # For Separating the Pages
 
+    open('static/application_output.txt', "w").close()
+    f = open('static/application_output.txt', 'w')
+    f.write(name + '\n')
+    f.write(address+ '\n')
+    f.write(phonenum+ '\n')
+    f.write(email+ '\n')
+    f.write(info+ '\n')
+
     # closing the pdf file object
     pdfFileObj.close()
-    return render_template('display.html', usr = user, name = name, phonenum = phonenum, address = address, info = info )
+    return render_template('display.html',
+                           usr=user,
+                           name=name,
+                           phonenum=phonenum,
+                           address=address,
+                           info=info)
 
 
 if __name__ == '__main__':
